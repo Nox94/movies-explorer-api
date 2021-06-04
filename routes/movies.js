5d893edc2114e9ed64eb6260ccf6b57094970389
Евгения <69312077+Nox94@users.noreply.github.com>
2021-06-04 14:01:06 +0300

    создает роуты п-лей и фильмов

---
+++ routes/movies.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+//тут будут подключены контроллеры для фильмов
+
+router.get('/movies', _methodname);
+router.post('/movies', _methodname);
+router.delete('/movies/:movieId', _methodname);
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+// тут будут подключены контроллеры для роутов
+
+router.get('/me', _methodname);
+router.patch('/me', _methodname);
