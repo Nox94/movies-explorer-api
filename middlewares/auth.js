8378203950623076456bcf5c5613b2f14a709f53
Evgenia Amelina <shkarina.evgenia@yandex.ru>
2021-06-06 12:07:22 +0300

    создает мидлвару авторизации

---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const Unauthorized = require('../errors/Unauthorized'); // 401
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization.replace('Bearer ', '');
+  // console.log(req.headers.authorization);
+  // console.log(token);
+  if (!token) {
+    next(new Unauthorized('Необходима авторизация.'));
+  } else {
+    let payload;
+    try {
+      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    } catch (err) {
+      next(new Unauthorized('Необходима авторизация.'));
+    }
+    req.user = payload;
+    next();
+  }
+};
