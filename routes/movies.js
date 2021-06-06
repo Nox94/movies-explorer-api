2533d360c416dcf6d7bea6a4ffd3bcd613c83804
Evgenia Amelina <shkarina.evgenia@yandex.ru>
2021-06-06 11:50:59 +0300

    делает валидацию роутов movies с celebrate/joi

--- routes/movies.js
+++ routes/movies.js
@@ -16,21 +16,41 @@ router.post('/movies', celebrate({
     duration: Joi.number().required().min(2).max(3),
     year: Joi.string().required().min(4).max(4),
     description: Joi.string().required().min(2).max(200),
-    image: {
-      url: Joi.string()
-        .required().custom((value, helpers) => {
-          const isValid = validator.isURL(value, { require_protocol: true });
-          if (isValid) {
-            return value;
-          }
-          return helpers.message('Введены некорректные данные ссылки на изображение.');
-        }),
-    },
-    trailer,
-    nameRU,
-    nameEN,
-    thumbnail,
-    movieId,
+    image: Joi.string()
+      .required().custom((value, helpers) => {
+        const isValid = validator.isURL(value, { require_protocol: true });
+        if (isValid) {
+          return value;
+        }
+        return helpers.message('Введены некорректные данные ссылки на изображение.');
+      }),
+    trailer: Joi.string()
+      .required().custom((value, helpers) => {
+        const isValid = validator.isURL(value, { require_protocol: true });
+        if (isValid) {
+          return value;
+        }
+        return helpers.message('Введены некорректные данные ссылки на трейлер.');
+      }),
+    nameRU: Joi.string().required().min(2).max(50)
+      .regex(/^[А-ЯЁ][а-яё]*$/),
+    nameEN: Joi.string().required().min(2).max(50)
+      .regex(/^[a-zA-Z]*$/),
+    thumbnail: Joi.string()
+      .required().custom((value, helpers) => {
+        const isValid = validator.isURL(value, { require_protocol: true });
+        if (isValid) {
+          return value;
+        }
+        return helpers.message('Введены некорректные данные ссылки на миниатюру изображения.');
+      }),
+    movieId: Joi.number().required().min(1),
   }),
 }), createMovie);
-router.delete('/movies/:movieId', deleteMovieById);
+router.delete('/movies/:movieId', celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.number().required().min(1),
+  }),
+}), deleteMovieById);
+
+module.exports = router;
