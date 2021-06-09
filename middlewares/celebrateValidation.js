96148d05d80c952a38f7f2da693c6710d4c1443a
Evgenia Amelina <shkarina.evgenia@yandex.ru>
2021-06-09 15:40:34 +0300

    добавляет email в валидацию на updateUser

--- middlewares/celebrateValidation.js
+++ middlewares/celebrateValidation.js
@@ -55,6 +55,7 @@ const validateMovieObjectId = celebrate({
 const validateUsersBody = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().min(2).max(30),
   }),
 });
 
